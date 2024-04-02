4710da72e12b66e8797cd0cb0341893441e52966
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-04-02 00:23:59 -0300

    ✨ allow configuring odatabind for shadow properties in reference collections

---
+++ Cblx.EntityFrameworkCore.Dataverse.Tests/Data/SubCollectionWithDivergentNavigationName/ThingConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Cblx.EntityFrameworkCore.Dataverse.Tests.Data.SubCollectionWithDivergentNavigationName;
+
+public class ThingConfiguration : IEntityTypeConfiguration<Thing>
+{
+    public void Configure(EntityTypeBuilder<Thing> builder)
+    {
+        builder.ToTable("creec_thing");
+        builder.ToEntitySet("creec_things");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        builder.Property(x => x.Id).HasColumnName("creec_thingid");
+        builder.Property(x => x.Name).HasColumnName("new_name");
+
+        builder.HasMany(x => x.Children)
+               .WithOne()
+               .IsRequired()
+               .HasForeignKey("creec_parent")
+               .HasODataBindPropertyName("creec_wrong_name")
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Cblx.EntityFrameworkCore.Dataverse/ReferenceCollectionBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cblx.EntityFrameworkCore.Dataverse;
+public static class ReferenceCollectionBuilderExtensions
+{
+    public static ReferenceCollectionBuilder<TPrincipalEntity, TDependentEntity> HasODataBindPropertyName<TPrincipalEntity, TDependentEntity>(
+        this ReferenceCollectionBuilder<TPrincipalEntity, TDependentEntity> referenceCollectionBuilder, string value)
+        where TDependentEntity : class
+        where TPrincipalEntity : class
+    {
+        referenceCollectionBuilder.Metadata.PrincipalToDependent!.ForeignKey.Properties[0].AddAnnotation("ODataBindPropertyName", value);
+        return referenceCollectionBuilder.HasAnnotation("ODataBindPropertyName", value);
+    }
+}
