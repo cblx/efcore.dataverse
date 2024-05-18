7ed8e39ae2f239ec3148cbef0c89ac2de0084d83
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-05-18 18:28:53 -0300

    fix converter

--- Cblx.EntityFrameworkCore.Dataverse/DataverseValueConverter.cs
+++ Cblx.EntityFrameworkCore.Dataverse/DataverseValueConverter.cs
@@ -3,17 +3,17 @@ using System.Linq.Expressions;
 
 namespace Cblx.EntityFrameworkCore.Dataverse;
 
-public class DataverseValueConverter<TModel, TProvider> : ValueConverter<TModel, TProvider>, IDataverseValueConverter
+public class DataverseValueConverter<TModel, TProvider, TDataverseWebApi> : ValueConverter<TModel, TProvider>, IDataverseValueConverter
 {
     public DataverseValueConverter(
         Expression<Func<TModel, TProvider>> convertToProviderExpression,
         Expression<Func<TProvider, TModel>> convertFromProviderExpression,
-        Func<object?, object?> convertToDataverseWebApi,
+        Func<TModel?, TDataverseWebApi?> convertToDataverseWebApi,
         ConverterMappingHints? mappingHints = null) : base(convertToProviderExpression, 
                                                            convertFromProviderExpression, 
                                                            mappingHints)
     {
-        ConvertToDataverseWebApi = convertToDataverseWebApi;
+        ConvertToDataverseWebApi = from => convertToDataverseWebApi((TModel?)from);
     }
 
     public Func<object?, object?> ConvertToDataverseWebApi { get; }
